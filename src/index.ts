b19899782ff488f63607206a5ea96f5503285dc3
Hosain <cryptichasan925@gmail.com>
2024-10-29 01:00:29 +0600

    creat and get done

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+ import { PrismaClient } from "@prisma/client";
+
+ const prisma = new PrismaClient();
+
+ const main = async () => {
+    // const result = await prisma.post.create({
+    //     data : {
+    //         title : "this is title",
+    //         content : "this is content",
+    //         authorName : "Fahim ahmed "
+    //     }
+    // });
+    // console.log(result)
+    const getAllFromDB = await prisma.post.findMany();
+    console.log(getAllFromDB)
+ }
+
+ main();
