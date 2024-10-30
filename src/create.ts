31d85b54403381f638135c192751de1644854176
Hosain <cryptichasan925@gmail.com>
2024-10-30 23:22:55 +0600

    create and create many

---
+++ src/create.ts
@@ -0,0 +1,42 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const main = async () => {
+  // creat data into db
+  // const result = await prisma.post.create({
+  //     data : {
+  //         title : "this is title",
+  //         content : "this is content",
+  //         authorName : "Fahim ahmed "
+  //     }
+  // });
+  // console.log(result)
+  // const getAllFromDB = await prisma.post.findMany();
+  // console.log(getAllFromDB);
+// create many
+  const createMany = await prisma.post.createMany({
+    data : [
+        {
+            title : "this is title 1",
+          content : "this is content 1",
+           authorName : "author 1 "
+        },
+        {
+            title : "this is title 2",
+          content : "this is content 2",
+           authorName : "author 2 "
+        },
+        {
+            title : "this is title 3",
+          content : "this is content 3",
+           authorName : "author 3"
+        }
+    ]
+  })
+
+  console.log(createMany)
+
+};
+
+main();
